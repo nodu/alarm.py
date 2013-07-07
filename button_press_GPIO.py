9ced4a5b026c6b6598ecf7f2576eecbe27445dfb
Matt Nodurfth <mnodurft@gmail.com>
2013-07-07 19:17:48 +0800

    Initial Commit

---
+++ button_press_GPIO.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+# Matt Nodurfth
+#Button wiriting: Bottom Left GND, Top Right Pin 23 and 10k resistor, 10k Resistor goes inbetween 3.3V and tied with 23
+
+# Add buttonPressGPIO.py to startup
+# Some sources suggest adding to rc.local isn't a best practice.  I'm looking into daemonizing the process
+# python /home/pi/.mpd/button_press_GPIO.py >> /home/pi/.mpd/button.log 2>&1 &
+
+#from time import sleep
+import time
+import re, commands, RPi.GPIO as GPIO # Should get used to using subprocess as commands is depricated
+import mpd_alarm_proper as alarm
+
+state_list = [ 'mpc clear', 'mpc volume 100' , 'mpc repeat off', 'mpc random off', 'mpc single off', 'mpc consume off']
+
+GPIO.setmode(GPIO.BCM) # Sets the pin layout to BCM 
+GPIO.setup(23, GPIO.IN) # Init varible for GPIO BCM pin 23 as input
+button_press_length = 0 # Init varible
+
+def check_state():
+    (status, output) = commands.getstatusoutput('mpc status')
+    output_match = re.search(r'\[(\w+)\]', output)
+    return output_match
+print "This ish is starting -- ", time.strftime("%a, %d %b %Y %H:%M:%S")
+while True:
+    input = GPIO.input(23)
+    if input == False: #change the wiring of the switch and change back to True, or keep as is... WORKING False
+        if button_press_length == 0:
+            print "pressed!"
+            if check_state() == None:(status3, output3) = commands.getstatusoutput('mpc play')
+            elif check_state().group(1) == 'playing':
+                (status1, output1) = commands.getstatusoutput('mpc pause')
+            else: (status2, output2) = commands.getstatusoutput('mpc play')
+        elif button_press_length > 3: # Still outputs a press of length 0 before printing the long press, need to prevent the first length 0 press..
+            print "You pressed the button for longer than 3 seconds!"
+            alarm.set_state(state_list)
+            alarm.add_random_playlist()
+            alarm.start_mpd()
+            button_press_length = 0
+        
+        button_press_length += 0.05        
+    else:
+        button_press_length = 0
+    
+    prev_input =  not input
+    time.sleep(0.05) # Prevents debounching
+    
+    
+#It's not recording the presses in the log started by rc.local...
---
+++ mpd_alarm.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# Uses mpd and mpc to play a random playlist starting at 6:35AM with increasing volume.
+# Matt Nodurfth
+
+# Setup: Add this python script to a cron with your desired start time, path can be anywhere
+# 35 6 * * 1-5 /home/pi/.mpd/alarm.py To start at 6:35AM M-F
+# Change varibles based on your directory structure and desired increments
+
+# Add a script to add this program to crontab?
+
+import os, time, commands, random, sys, re
+
+playlist_dir = '/home/pi/.mpd/playlists'
+alarm_log = '/home/pi/.mpd/pythonAlarm/mpd_alarm.log' 
+mpd_volume = 60 # Initial volume setting
+step_vol = 10 # Increase volume by 10%
+step_increment = 150 # Increases volume every 150 seconds
+
+# Sets the initial state of MPD.  Clears the playlist; sets volume; and turns repeat, random, single, and consume off
+state_list = [ 'mpc clear', 'mpc volume ' + str(mpd_volume), 'mpc repeat off', 'mpc random off', 'mpc single off', 'mpc consume off']
+
+def log(cmd, status, output):
+    """Function for logging errors with timestamp"""
+    if status != 0: 
+        log = open(alarm_log, 'a')
+        log.write(time.strftime("%a, %d %b %Y %H:%M:%S") + ' --  ' + cmd + ': ' + output +'\n')
+        log.close()
+        
+def set_state(state):
+    """Set initial state"""
+    for cmd in state:
+        (status, output) = commands.getstatusoutput(cmd)
+        log(cmd, status, output)
+
+def add_random_playlist():
+    """Chose a random playlist"""
+    cmd = 'mpc load '
+    playlist_list = os.listdir(playlist_dir)
+    if not playlist_list:
+        log('playlist', 1, 'NO PLAYLISTS!!!')
+        sys.exit(1)
+    random_num = random.choice(playlist_list)
+    album_name = re.sub(' ', '\ ', random_num)
+    (status, output) = commands.getstatusoutput(cmd + album_name)
+    log(cmd, status, output)
+    
+def start_mpd():
+    """Start mpd""" 
+    cmd = 'mpc play'
+    (status, output) = commands.getstatusoutput(cmd)
+    log(cmd, status, output)
+
+def vol_increase(mpd_volume, step_vol, step_increment):
+    """Increase volume by 10 every 2.5 minutes"""
+    cmd = 'mpc volume'
+    while mpd_volume < 101:
+        time.sleep(step_increment)
+        (status, output) = commands.getstatusoutput('mpc volume ' + str(mpd_volume))
+        log(cmd, 0, output)
+        mpd_volume += step_vol
+        
+def main():
+    set_state(state_list)
+    add_random_playlist()
+    start_mpd()
+    vol_increase(mpd_volume, step_vol, step_increment)
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
